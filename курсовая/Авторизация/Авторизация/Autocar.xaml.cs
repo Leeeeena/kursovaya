49ec22b1d76ab68d35c7d024d93585f4ca6105b9
Leeeeena <70923151+Leeeeena@users.noreply.github.com>
2021-02-23 16:53:36 +0900

    Add files via upload

---
+++ курсовая/Авторизация/Авторизация/Autocar.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Авторизация
+{
+    /// <summary>
+    /// Логика взаимодействия для Autocar.xaml
+    /// </summary>
+    public partial class Autocar : Window
+    {
+        public Autocar()
+        {
+            InitializeComponent();
+        }
+
+        private void gotovo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
